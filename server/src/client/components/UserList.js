ccb3ad4bbcb1c79c2472aee6f1e737fac1d1c364
yagopv <yagoperez@outlook.com>
2018-03-12 22:18:14 +0100

    Adding reducers and store

---
+++ server/src/client/components/UserList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../actions';
+
+class UserList extends Component {
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
+
+  renderUsers() {
+    return this.props.users.map(user => {
+      return <li key={user.id}>{user.name}</li>
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Here´s a big list of users
+        <ul>{this.renderUsers()}</ul>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({users}) => ({ users });
+
+export default connect(mapStateToProps, { fetchUsers })(UserList);
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,21 @@
+//async-await (action creators) not working without polyfill
+import 'babel-polyfill';
+
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  res.send(renderer(req));
+  const store = createStore();
+
+  //Some logic to initialize
+  //and load data in the store
+
+  res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
