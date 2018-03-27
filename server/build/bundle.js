5c5edce777c2b6fb47682989c6178474d04aafda
yagopv <yagoperez@outlook.com>
2018-03-27 23:59:38 +0200

    configure proxy

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -192,6 +192,10 @@ var _express = __webpack_require__(8);
 
 var _express2 = _interopRequireDefault(_express);
 
+var _expressHttpProxy = __webpack_require__(20);
+
+var _expressHttpProxy2 = _interopRequireDefault(_expressHttpProxy);
+
 var _reactRouterConfig = __webpack_require__(1);
 
 var _Routes = __webpack_require__(2);
@@ -202,15 +206,22 @@ var _renderer = __webpack_require__(12);
 
 var _renderer2 = _interopRequireDefault(_renderer);
 
-var _createStore = __webpack_require__(15);
+var _createStore = __webpack_require__(16);
 
 var _createStore2 = _interopRequireDefault(_createStore);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-//async-await (action creators) not working without polyfill
-var app = (0, _express2.default)();
+var app = (0, _express2.default)(); //async-await (action creators) not working without polyfill
 
+
+app.use('/api', (0, _expressHttpProxy2.default)('http://react-ssr-api.herokuapp.com', {
+  // This is only for Google Auth purposes
+  proxyReqOptOperator: function proxyReqOptOperator(opts) {
+    opts.header['x-forwarded-host'] = 'localhost:3000';
+    return opts;
+  }
+}));
 app.use(_express2.default.static('public'));
 
 app.get('*', function (req, res) {
@@ -402,7 +413,7 @@ var _reactRedux = __webpack_require__(3);
 
 var _reactRouterConfig = __webpack_require__(1);
 
-var _serializeJavascript = __webpack_require__(19);
+var _serializeJavascript = __webpack_require__(15);
 
 var _serializeJavascript2 = _interopRequireDefault(_serializeJavascript);
 
@@ -448,6 +459,12 @@ module.exports = require("react-router-dom");
 
 /***/ }),
 /* 15 */
+/***/ (function(module, exports) {
+
+module.exports = require("serialize-javascript");
+
+/***/ }),
+/* 16 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -459,11 +476,11 @@ Object.defineProperty(exports, "__esModule", {
 
 var _redux = __webpack_require__(5);
 
-var _reduxThunk = __webpack_require__(16);
+var _reduxThunk = __webpack_require__(17);
 
 var _reduxThunk2 = _interopRequireDefault(_reduxThunk);
 
-var _reducers = __webpack_require__(17);
+var _reducers = __webpack_require__(18);
 
 var _reducers2 = _interopRequireDefault(_reducers);
 
@@ -476,13 +493,13 @@ exports.default = function () {
 };
 
 /***/ }),
-/* 16 */
+/* 17 */
 /***/ (function(module, exports) {
 
 module.exports = require("redux-thunk");
 
 /***/ }),
-/* 17 */
+/* 18 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -494,7 +511,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _redux = __webpack_require__(5);
 
-var _usersReducer = __webpack_require__(18);
+var _usersReducer = __webpack_require__(19);
 
 var _usersReducer2 = _interopRequireDefault(_usersReducer);
 
@@ -505,7 +522,7 @@ exports.default = (0, _redux.combineReducers)({
 });
 
 /***/ }),
-/* 18 */
+/* 19 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -530,10 +547,10 @@ exports.default = function () {
 };
 
 /***/ }),
-/* 19 */
+/* 20 */
 /***/ (function(module, exports) {
 
-module.exports = require("serialize-javascript");
+module.exports = require("express-http-proxy");
 
 /***/ })
 /******/ ]);
--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@
 import 'babel-polyfill';
 
 import express from 'express';
+import proxy from 'express-http-proxy';
 import { matchRoutes } from 'react-router-config';
 import Routes from './client/Routes';
 import renderer from './helpers/renderer';
@@ -9,6 +10,16 @@ import createStore from './helpers/createStore';
 
 const app = express();
 
+app.use(
+  '/api',
+  proxy('http://react-ssr-api.herokuapp.com', {
+    // This is only for Google Auth purposes
+    proxyReqOptOperator(opts) {
+      opts.header['x-forwarded-host'] = 'localhost:3000';
+      return opts;
+    }
+  })
+);
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
